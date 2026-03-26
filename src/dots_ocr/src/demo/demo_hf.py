caa71941735123e18f45e0c385d6d109d4699319
SteffRhes <steffrhes@pm.me>
2026-03-26 11:15:34 +0100

    dots_ocr_5_german_extensive: recreate state of commit 7324f140b689e3af9f2e5e40fade12c640df3b27 at internal repo

--- src/dots_ocr/src/demo/demo_hf.py
+++ src/dots_ocr/src/demo/demo_hf.py
@@ -2,14 +2,25 @@ import os
 if "LOCAL_RANK" not in os.environ:
     os.environ["LOCAL_RANK"] = "0"
 from glob import glob
-
+import pickle
+from dots_ocr.utils import dict_promptmode_to_prompt
 import torch
 from transformers import AutoModelForCausalLM, AutoProcessor, AutoTokenizer
 from qwen_vl_utils import process_vision_info
-from dots_ocr.utils import dict_promptmode_to_prompt
 
-def inference(image_path, prompt, model, processor):
-    # image_path = "demo/demo_image1.jpg"
+
+INFERENCE_ENABLED = True
+MAX_NEW_TOKENS = 8000
+
+PROMPT = """
+Das ist ein Scan einer deutschen historischen Zeitung aus dem frühen 20. Jahrhundert. 
+Bitte führe OCR darauf aus, also extrahiere den Text und behalte dabei die Leserichtung bei.
+Beachte auch, dass die Schrift in Fraktur gehalten ist. Der Output soll nur der Text alleine sein.
+"""
+
+
+def inference(image_path, prompt, model, processor, image_id):
+    print("-- inference")
     messages = [
         {
             "role": "user",
@@ -23,52 +34,63 @@ def inference(image_path, prompt, model, processor):
         }
     ]
 
+    if INFERENCE_ENABLED:
+        # Preparation for inference
+        text = processor.apply_chat_template(
+            messages, 
+            tokenize=False, 
+            add_generation_prompt=True
+        )
+        image_inputs, video_inputs = process_vision_info(messages)
+        inputs = processor(
+            text=[text],
+            images=image_inputs,
+            videos=video_inputs,
+            padding=True,
+            return_tensors="pt",
+        )
+        inputs = inputs.to("cuda")
 
-    # Preparation for inference
-    text = processor.apply_chat_template(
-        messages, 
-        tokenize=False, 
-        add_generation_prompt=True
-    )
-    image_inputs, video_inputs = process_vision_info(messages)
-    inputs = processor(
-        text=[text],
-        images=image_inputs,
-        videos=video_inputs,
-        padding=True,
-        return_tensors="pt",
-    )
-
-    inputs = inputs.to("cuda")
-
-    # Inference: Generation of the output
-    generated_ids = model.generate(**inputs, max_new_tokens=24000)
-    generated_ids_trimmed = [
-        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-    ]
-    output_text = processor.batch_decode(
-        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-    )
+        # Inference: Generation of the output
+        generated_ids = model.generate(**inputs, max_new_tokens=MAX_NEW_TOKENS)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+        output_text = processor.batch_decode(
+            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+    else:
+        output_text = "test"
     print(output_text)
-
+    output_path = f"/pressmint-ground-truth/data/texts/dots_ocr_4/{image_id}.pkl"
+    print(f"{output_path=}")
+    with open(output_path, "wb") as f:
+        pickle.dump(output_text, f)
 
 
 if __name__ == "__main__":
     # We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
-    model_path = "./weights/DotsOCR"
-    model = AutoModelForCausalLM.from_pretrained(
-        model_path,
-        attn_implementation="flash_attention_2",
-        torch_dtype=torch.bfloat16,
-        device_map="auto",
-        trust_remote_code=True
-    )
-    processor = AutoProcessor.from_pretrained(model_path,  trust_remote_code=True)
+    print("--- main")
+    if INFERENCE_ENABLED:
+        model_path = "./weights/DotsOCR"
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            attn_implementation="flash_attention_2",
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            trust_remote_code=True
+        )
+        processor = AutoProcessor.from_pretrained(model_path,  trust_remote_code=True)
+    else:
+        model = None
+        processor = None
+    print("--- model and processor loaded")
 
-    # image_path = "demo/demo_image1.jpg"
-    for image_path in glob("/pressmint-ground-truth/data/texts/images/*"):
+    folder = "/pressmint-ground-truth/data/texts/images/"
+    for image_file_name in os.listdir(folder):
+        image_path = folder + image_file_name
+        print("--- new image")
         print(f"{image_path=}")
-        for prompt_mode, prompt in dict_promptmode_to_prompt.items():
-            print(f"prompt: {prompt}")
-            inference(image_path, prompt, model, processor)
+        image_id = image_file_name.replace(".jpg", "")
+        inference(image_path, PROMPT, model, processor, image_id)
     
